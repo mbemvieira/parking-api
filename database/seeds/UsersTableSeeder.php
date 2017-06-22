b3ef1b8c73d29bbb8b17292f5bfe5161c608d45a
Mateus Vieira <desenvolvimento@ortep.com.br>
2017-06-22 08:49:53 -0300

    Seeder fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,23 +23,5 @@ class UsersTableSeeder extends Seeder
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'Guilherme Correa',
-            'email' => 'guilhermecorrea87@gmail.com',
-            'password' => bcrypt('CCVdWV7bR7'),
-            'remember_token' => str_random(10),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Vitor',
-            'email' => 'vitor@email.com',
-            'password' => bcrypt('PbJCLu3OXm'),
-            'remember_token' => str_random(10),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
-
     }
 }
